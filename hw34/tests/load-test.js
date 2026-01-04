195701f8f96d37d4a9615b4a50cd0f58b0ce543d
AnnaNik334743 <annanikiforova564@gmail.com>
2026-01-04 23:01:57 +0300

    nginx & vertical scaling

--- hw34/tests/load-test.js
+++ hw34/tests/load-test.js
@@ -1,22 +1,23 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+
 
 export const options = {
   scenarios: {
-    high_traffic: {
+    constant_1000: {
       executor: 'constant-arrival-rate',
-      rate: 300,
+      rate: 1000,
       timeUnit: '1s',
       duration: '2m',
-      preAllocatedVUs: 50,
-    },
+      preAllocatedVUs: 200,
+      maxVUs: 500,
+    }
   },
   thresholds: {
     http_req_duration: ['p(95)<500'],
   },
 };
 
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'http://localhost:8080';
 
 const endpoints = [
   { path: '/', weight: 1 },
@@ -41,5 +42,4 @@ function getRandomEndpoint() {
 export default function () {
   const endpoint = getRandomEndpoint();
   const response = http.get(`${BASE_URL}${endpoint}`);
-  sleep(0.01);
 }
